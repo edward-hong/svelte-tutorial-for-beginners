d8c3e5cf81b1ea83fbddc3290a91f18773241f5e
Edward Hong <edward.hong527@gmail.com>
2021-04-01 19:41:20 +1300

    V07 Inline Event Handlers

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,10 @@
     { name: 'mario', beltColour: 'orange', age: 45, id: 2 },
     { name: 'luigi', beltColour: 'brown', age: 35, id: 3 }
   ]
+
+	const handleClick = id => {
+		people = people.filter((person) => person.id !== id)
+	}
 </script>
 
 <main>
@@ -11,6 +15,7 @@
 		<div>
 			<h4>{person.name}</h4>
 			<p>{person.age} years old, {person.beltColour} belt.</p>
+			<button on:click={() => handleClick(person.id)}>delete</button>
 		</div>
 	{:else}
 		<p>There are no people to show...</p>
