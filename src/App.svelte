f3b5735eaa7431dd404246af16823f17528b208a
Edward Hong <edward.hong527@gmail.com>
2021-04-01 19:51:23 +1300

    V09 Components

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+	import Modal from './Modal.svelte'
+
 	let people = [
     { name: 'yoshi', beltColour: 'black', age: 25, id: 1 },
     { name: 'mario', beltColour: 'orange', age: 45, id: 2 },
@@ -8,18 +10,9 @@
 	const handleClick = id => {
 		people = people.filter((person) => person.id !== id)
 	}
-
-	let num = 3
 </script>
 
-{#if num > 20}
-	<p>Greater than 20</p>
-{:else if num > 5}
-	<p>Greater than 5</p>
-{:else}
-	<p>Not greater than 5</p>
-{/if}
-
+<Modal />
 <main>
 	{#each people as person (person.id)}
 		<div>
