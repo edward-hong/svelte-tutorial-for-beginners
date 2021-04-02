2c0af93fbc1d45fde8a688e811997b5ed4fbdb11
Edward Hong <edward.hong527@gmail.com>
2021-04-02 19:55:48 +1300

    V34 Tweens & Animations

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { tweened } from 'svelte/motion'
+
   import Card from '../shared/Card.svelte'
   import Button from '../shared/Button.svelte'
 
@@ -6,10 +8,16 @@
 
   export let poll
 
+  // tweened percentages
+  const tweenedA = tweened(0)
+  const tweenedB = tweened(0)
+
   // reactive values
   $: totalVotes = poll.votesA + poll.votesB
-  $: percentA = Math.floor(100 / totalVotes * poll.votesA)
-  $: percentB = Math.floor(100 / totalVotes * poll.votesB)
+  $: percentA = Math.floor(100 / totalVotes * poll.votesA) || 0
+  $: percentB = Math.floor(100 / totalVotes * poll.votesB) || 0
+  $: tweenedA.set(percentA)
+  $: tweenedB.set(percentB)
 
   // handling votes
   const handleVote = (option, id) => {
@@ -41,11 +49,11 @@
     <h3>{poll.question}</h3>
     <p>Total Votes: {totalVotes}</p>
     <div class="answer" on:click={() => handleVote('a', poll.id)}>
-      <div class="percent percent-a" style="width: {percentA}%"></div>
+      <div class="percent percent-a" style="width: {$tweenedA}%"></div>
       <span>{poll.answerA} ({poll.votesA})</span>
     </div>
     <div class="answer" on:click={() => handleVote('b', poll.id)}>
-      <div class="percent percent-b" style="width: {percentB}%"></div>
+      <div class="percent percent-b" style="width: {$tweenedB}%"></div>
       <span>{poll.answerB} ({poll.votesB})</span>
     </div>
     <div class="delete">
--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { fade, slide, scale } from 'svelte/transition'
+  import { fade, scale } from 'svelte/transition'
   import { flip } from 'svelte/animate'
 
   import PollDetails from './PollDetails.svelte'
